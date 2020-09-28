d06e49455502dd4d47954323723f5855d20bacd0
oribanda <oribanda2@gmail.com>
2020-09-28 17:53:47 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2020_09_28_084743_create_classes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('member');
+            $table->string('avatar')->default('noimage.png');
+            $table->integer('user_id')->unsigned();
+            $table->integer('admin_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('admin_id')
+                ->references('id')
+                ->on('admins')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
--- database/migrations/2020_09_28_085159_create_genders_table.php
+++ database/migrations/2020_09_28_085159_create_genders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGendersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar')->default('noimage.png');
-            $table->rememberToken();
+            $table->string('man');
+            $table->string('woman');
+            $table->integer('song_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('song_id')
+                ->references('id')
+                ->on('songs')
+                ->onDelete('cascade');
         });
     }
 
@@ -32,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genders');
     }
 }
